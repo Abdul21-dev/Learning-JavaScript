4eb2bf480bf95307a87fa3748bc8bb6683289eb4
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-02-19 19:42:27 +0530

    Template literals

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ console.log("Hello JavaScript");
 let a=5;
 let b=3;
 console.log("sum is : ", a + b);
+let pencilPrice = 10;
+let eraserPrice = 5;
+console.log(`Total price is ${pencilPrice + eraserPrice} Rupees`);
