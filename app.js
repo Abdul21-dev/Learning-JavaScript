38ee1a02b056857a3181656b165fd1fc85494ecf
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-02-19 19:54:04 +0530

    Using operators

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ console.log("Hello JavaScript");
 let a=5;
 let b=3;
 console.log("sum is : ", a + b);
+// Template literals
 let pencilPrice = 10;
 let eraserPrice = 5;
 console.log(`Total price is ${pencilPrice + eraserPrice} Rupees`);
+// Operators in Js
+console.log(a++); // 5
+console.log(a--); // 7
