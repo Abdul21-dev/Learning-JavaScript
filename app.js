a830e8538a138fb6a728d0880d5e68df5136a3f1
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-02-20 21:26:42 +0530

    Comparison operators

--- app.js
+++ app.js
@@ -9,3 +9,6 @@ console.log(`Total price is ${pencilPrice + eraserPrice} Rupees`);
 // Operators in Js
 console.log(a++); // 5
 console.log(a--); // 7
+// comparison operators 
+let age = 16;
+console.log(age>16);
